956decde409e2c2e2e4e88e135ad68c29dce02bc
tanka2u <ptanka123@gmail.com>
2024-08-14 13:44:11 +0200

    ready for deployment

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb://localhost:27017/meetups"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
---
+++ pages/index.js
@@ -0,0 +1,50 @@
+import { MongoClient } from "mongodb";
+import Head from "next/head";
+import MeetupList from "../components/meetups/MeetupList";
+import { Fragment } from "react";
+
+function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetup!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect("mongodb://localhost:27017/meetups");
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
